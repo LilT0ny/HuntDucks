cb8434b2848377934d26b7899b051cb110a84271
___whoisanthony___ <anthony.gomez@epn.edu.ec>
2025-05-16 10:44:58 -0500

    ADD Button Login

--- app/src/main/java/com/anthony/gomez/huntducks/LoginActivity.kt
+++ app/src/main/java/com/anthony/gomez/huntducks/LoginActivity.kt
@@ -1,21 +1,34 @@
 package com.anthony.gomez.huntducks
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
+    private lateinit var buttonLogin : Button;
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_login2)
+        buttonLogin = findViewById<Button>(R.id.buttonLogin);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        buttonLogin.setOnClickListener{
+            startActivity(Intent(this,MainActivity::class.java))
+        }
     }
 }
+
+
+
+
+
